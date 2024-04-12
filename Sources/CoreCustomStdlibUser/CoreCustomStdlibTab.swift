494cf9f9246a755def51397f482244d32628cd4c
Alex Lorenz <arphaman@gmail.com>
2024-04-12 12:05:24 -0700

    split libcxx utils into a file

--- Sources/CoreCustomStdlibUser/CoreCustomStdlibTab.swift
+++ Sources/CoreCustomStdlibUser/CoreCustomStdlibTab.swift
@@ -24,35 +24,3 @@ class TabImplWithCustomCxxStdlib: Tab {
         cxxTab.visitPage(std.string(url))
     }
 }
-
-extension std.string {
-  /// Creates a C++ string having the same content as the given Swift string.
-  ///
-  /// - Complexity: O(*n*), where *n* is the number of UTF-8 code units in the
-  ///   Swift string.
-  init(_ string: String) {
-    self.init()
-    for char in string.utf8 {
-      self.push_back(value_type(bitPattern: char))
-    }
-  }
-}
-
-extension String {
-  /// Creates a String having the same content as the given C++ string.
-  ///
-  /// If `cxxString` contains ill-formed UTF-8 code unit sequences, this
-  /// initializer replaces them with the Unicode replacement character
-  /// (`"\u{FFFD}"`).
-  ///
-  /// - Complexity: O(*n*), where *n* is the number of bytes in the C++ string.
-  init(_ cxxString: std.string) {
-    let buffer = UnsafeBufferPointer<CChar>(
-      start: cxxString.__c_strUnsafe(),
-      count: cxxString.size())
-    self = buffer.withMemoryRebound(to: UInt8.self) {
-      String(decoding: $0, as: UTF8.self)
-    }
-    withExtendedLifetime(cxxString) {}
-  }
-}
